1fcc514dc70aad4ea5d61d487db7c1afad3c2ed8
olegmlsn <iolegmlsn@gmail.com>
2014-03-29 16:10:48 +0500

    Hash options

--- pleyada.rb
+++ pleyada.rb
@@ -11,12 +11,12 @@ class Pleyada
 
   def put(path, file)
     File.open(file, 'r') do |stream|
-      request('PUT', path, stream, File.size(file))
+      request(method: 'PUT', path: path, stream: stream, length: File.size(file))
     end
   end
 
   def get(path, file)
-    result = request('GET', path)
+    result = request(method: 'GET', path: path)
     if result
       File.open(file, 'w') do |file|
         file << result
@@ -25,26 +25,25 @@ class Pleyada
   end
 
   def mkcol(path)
-    request('Mkcol', path)
+    request(method: 'MKCOL', path: path)
   end
 
   def delete(path)
-    request('DELETE', path)
+    request(method: 'DELETE', path: path)
   end
 
   protected
 
-  #TODO use hash options
   #TODO add some magick
-  def request(method, path, stream = nil, length = nil)
-    case method
+  def request(options)
+    case options[:method]
     when 'PUT' then
-      req = Net::HTTP::Put.new(path)
+      req = Net::HTTP::Put.new(options[:path])
       req['Host'] = 'webdav.yandex.ru'
       req.content_type = 'application/octet-stream'
       req.basic_auth(@login, @password)
-      req.content_length = length
-      req.body_stream = stream
+      req.content_length = options[:length]
+      req.body_stream = options[:stream]
       result = @http.request(req)
       if result.is_a?(Net::HTTPCreated)
         return true
@@ -52,7 +51,7 @@ class Pleyada
         return false
       end
     when 'GET' then
-      req = Net::HTTP::Get.new(path)
+      req = Net::HTTP::Get.new(options[:path])
       req['Host'] = 'webdav.yandex.ru'
       req.basic_auth(@login, @password)
       result = @http.request(req)
@@ -62,7 +61,7 @@ class Pleyada
         return false
       end
     when 'MKCOL' then
-      req = Net::HTTP::Mkcol.new(path)
+      req = Net::HTTP::Mkcol.new(options[:path])
       req['Host'] = 'webdav.yandex.ru'
       req.basic_auth(@login, @password)
       result = @http.request(req)
@@ -72,7 +71,7 @@ class Pleyada
         return false
       end
     when 'DELETE' then
-      req = Net::HTTP::Delete.new(path)
+      req = Net::HTTP::Delete.new(options[:path])
       req['Host'] = 'webdav.yandex.ru'
       req.basic_auth(@login, @password)
       result = @http.request(req)
